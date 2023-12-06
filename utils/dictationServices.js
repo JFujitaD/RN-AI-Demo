7c9e41e1d60b39ada752a3a1da9b90dd915358b5
JFujitaD <fujitajulian@icloud.com>
2023-12-06 10:50:24 -0700

    Fixed environment variables

--- utils/dictationServices.js
+++ utils/dictationServices.js
@@ -2,5 +2,5 @@ const RECOGNIZE_ENDPOINT =
   'POST https://speech.googleapis.com/v1/speech:recognize';
 
 export const debug = () => {
-  console.log('API KEY:', 'TODO');
+  console.log('API KEY:', process.env.EXPO_PUBLIC_GOOGLE_API_KEY);
 };
