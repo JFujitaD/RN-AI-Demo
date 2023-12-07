7cbb689f674a18dc38c88f2e4206cd7d4b947676
JFujitaD <fujitajulian@icloud.com>
2023-12-07 07:50:52 -0700

    Tried adding scrolling

--- components/ChatList.jsx
+++ components/ChatList.jsx
@@ -1,14 +1,23 @@
 import { StyleSheet, FlatList } from 'react-native';
+import { useEffect, useRef } from 'react';
 
 import ChatItem from './ChatItem';
 
 export default function ChatList({ chats }) {
+  const flatListRef = useRef();
+
+  useEffect(() => {
+    console.log('SCROLLING TO THE BOTTOM');
+    flatListRef.current.scrollToEnd({ animated: true });
+  }, [chats]);
+
   const renderChatItem = (chatItem) => {
     return <ChatItem chatItem={chatItem} />;
   };
 
   return (
     <FlatList
+      ref={flatListRef}
       style={styles.root}
       data={chats}
       renderItem={({ item }) => renderChatItem(item)}
